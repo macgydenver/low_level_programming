4d522d8ff37a9657ef884b6bd8ac4b1d6a27ffdd
Wise Duho <delduwise@gmail.com>
2025-12-12 19:28:24 +0100

    Using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+* main - A program that prints a string of characters followed by a new line.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);
+}
